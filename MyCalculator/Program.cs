88b35b3f8fc0b207a319af7303d433c5eac80ecf
Антон Левин <despised01@gmail.com>
2022-12-26 21:01:23 +0300

    Добавьте файлы проекта.

---
+++ MyCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public interface ICalculator
+    {
+        void Calculate()
+        {
+            int 
+        }
+    }
+}
