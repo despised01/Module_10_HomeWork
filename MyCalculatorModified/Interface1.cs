b1c6459a33fc14dd112e3e74622a98c4e55ea32f
Антон Левин <despised01@gmail.com>
2022-12-26 21:45:31 +0300

    Добавлен интерфейс "ILogger" и  класс "Logger"

---
+++ MyCalculatorModified/Interface1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyCalculatorModified
+{
+    public interface ILogger
+    {
+        void Event(string Message);
+
+        void Error(string Message);
+    }
+
+    public class Logger: ILogger
+    {
+        public void Event(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(Message);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Error(string Message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(Message);
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ MyCalculatorModified/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace MyCalculatorModified
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    double Result = 0;
+
+                    ICalculator calculator = new Calculator();
+
+                    Console.WriteLine("Введите первое число: ");
+                    double Num1 = calculator.ReadNum();
+
+                    Console.WriteLine("Введите второе число: ");
+                    double Num2 = calculator.ReadNum();
+
+                    Result = calculator.Sum(Num1, Num2);
+
+                    calculator.Calculate(Result);
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("Некорректный ввод! Повторите попытку.");
+                }
+
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Число за пределами допустимого значения! Повторите попытку.");
+                }
+
+            }
+        }
+    }
+
+    public interface ICalculator
+    {
+        double Sum(double Num1, double Num2);
+
+        double ReadNum();
+
+        void Calculate(double Result);
+    }
+
+    public class Calculator : ICalculator
+    {
+        double ICalculator.Sum(double Num1, double Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        double ICalculator.ReadNum()
+        {
+            return Convert.ToDouble(Console.ReadLine());
+        }
+
+        void ICalculator.Calculate(double Result)
+        {
+            Console.WriteLine($"Ваш результат: {Result}");
+        }
+    }
+}
