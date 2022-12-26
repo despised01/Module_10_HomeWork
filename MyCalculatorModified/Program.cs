3c86ac8e5066f74cca5ad674ab4965aecddba6f1
Антон Левин <despised01@gmail.com>
2022-12-26 22:36:59 +0300

    Завершено написание программы к заданию 2

--- MyCalculatorModified/Program.cs
+++ MyCalculatorModified/Program.cs
@@ -1,38 +1,49 @@
 ﻿using System;
+using System.Threading;
 
 namespace MyCalculatorModified
 {
     class Program
     {
+        private static ILogger Logger { get; set; }
+
         static void Main(string[] args)
         {
             while (true)
             {
+                double Result = 0;
+
+                Logger = new Logger();
+
+                ICalculator calculator = new Calculator(Logger);
+
                 try
-                {
-                    double Result = 0;
-
-                    ICalculator calculator = new Calculator();
-
+                {      
                     Console.WriteLine("Введите первое число: ");
                     double Num1 = calculator.ReadNum();
 
                     Console.WriteLine("Введите второе число: ");
                     double Num2 = calculator.ReadNum();
 
+                    Thread.Sleep(250);
+
                     Result = calculator.Sum(Num1, Num2);
+
+                    Thread.Sleep(500);
 
                     calculator.Calculate(Result);
                 }
 
-                catch (FormatException)
+                catch (FormatException FoEx)
                 {
-                    Console.WriteLine("Некорректный ввод! Повторите попытку.");
+                    Logger.Error("Некорректный ввод! Повторите попытку.");
+                    Logger.Error(FoEx.Message);
                 }
 
-                catch (OverflowException)
+                catch (OverflowException OfEx)
                 {
-                    Console.WriteLine("Число за пределами допустимого значения! Повторите попытку.");
+                    Logger.Error("Число за пределами допустимого значения! Повторите попытку.");
+                    Logger.Error(OfEx.Message);
                 }
 
             }
@@ -50,14 +61,31 @@ namespace MyCalculatorModified
 
     public class Calculator : ICalculator
     {
+        private ILogger Logger { get; }
+
+        public Calculator(ILogger logger)
+        {
+            Logger = logger;
+        }
+
         double ICalculator.Sum(double Num1, double Num2)
         {
+            Logger.Event("Выполняется операция сложения...");
+
+            Thread.Sleep(250);
+
             return Num1 + Num2;
         }
 
         double ICalculator.ReadNum()
         {
-            return Convert.ToDouble(Console.ReadLine());
+            string NumRead = Console.ReadLine();
+
+            Logger.Event("Обработка...");
+
+            Thread.Sleep(500);
+
+            return Convert.ToDouble(NumRead);
         }
 
         void ICalculator.Calculate(double Result)
